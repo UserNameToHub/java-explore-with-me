aec2843a15e8f7c57b5b086831102a2c8e777915
aleksandrgineyka <meinepostkam@gmail.com>
2023-09-13 02:56:32 +0300

    style: удалить неиспользуемый импорт

--- stats-server/client/src/main/java/ru/practicum/stats/StatsController.java
+++ stats-server/client/src/main/java/ru/practicum/stats/StatsController.java
@@ -2,11 +2,8 @@ package ru.practicum.stats;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import ru.practicum.commonDto.HitDto;
 import ru.practicum.commonDto.HitGettingDto;
 import ru.practicum.stats.service.StatsService;
